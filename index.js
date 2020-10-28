a9bf83c7b1afd65c2ee8bd75e4f916e574d19ac3
김태형 <taeng9@MacBook-Pro.local>
2020-10-28 17:36:41 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 분석
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://AbangPa1ace:dyek7273!@boilerplate.kqsxb.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -13,6 +23,23 @@ mongoose.connect('mongodb+srv://AbangPa1ace:dyek7273!@boilerplate.kqsxb.mongodb.
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! Nodemon install completed!'))
+
+app.post('/register', (req, res) => {
+
+    //회원가입에 필요한 정보들을 Client에서 가져오면
+    //그것을 데이터베이스에 넣는다.
+
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({
+            success: false, err
+        })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
