f1a254f89ba588eb219056bc0366ad05ce7e3352
김태형 <taeng9@MacBook-Pro.local>
2020-11-04 18:38:42 +0900

    Concurrently, Redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'atnd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
